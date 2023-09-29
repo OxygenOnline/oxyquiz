30e03c690d7236ac97256bf94093aaa6d68ee707
Farkas Fruzsina Diána <fruzsinadiana@mailbox.unideb.hu>
2023-09-29 11:50:37 +0200

    post mapping done for quizzes

--- server/db/quizzes.js
+++ server/db/quizzes.js
@@ -99,7 +99,8 @@ const createQuiz = async (quiz, creator_id) => {
                 r.position
             ]);
             const resultId = resultResult.rows[0].id;
-            resultIdArray.push(resultId);
+            
+            resultIdArray.splice(r.position, 0, resultId);
         }
 
         // TODO: validate unique position for:
@@ -154,9 +155,6 @@ const createQuiz = async (quiz, creator_id) => {
 const updateQuizById = async (id, quiz) => {
     try {
 
-        // TODO: remove questions, results, options that are select * exist but not in the json
-        // add new questions, results, options that are not in select * but in json
-        // update existing ones
         const results = quiz.results;
         const questions = quiz.questions;
 
@@ -167,98 +165,189 @@ const updateQuizById = async (id, quiz) => {
         SET title = $1,
         description = $2,
         category_id = $3,
-        modified_at = NOW(),
+        modified_at = NOW()
         WHERE id = $4`;
         await pool.query(quizUpdate, [quiz.title, quizDesc, quiz.category_id, id]);
 
-        // const resultUpdate = `UPDATE result
-        // SET title = $1,
-        // description = $2
-        // WHERE id = $3
-        // AND quiz_id = $4;`;
-        // const resultQuery = "SELECT id FROM result WHERE question_id = $1";
-        // const resultInsert = `INSERT INTO result(quiz_id, title, description, position)
-        // VALUES ($1, $2, $3, $4);`;
-        // const resultDelete = "DELETE FROM result WHERE id = $1 AND question_id = $2";
-        // const resultResult = await pool.query(resultQuery, [id]);
-        // const existingResult = resultResult.rows.map(item => item.id);
+        const resultUpdate = `UPDATE result
+        SET title = $1,
+        description = $2,
+        position = $3
+        WHERE id = $4
+        AND quiz_id = $5;`;
+        const resultQuery = "SELECT id FROM result WHERE quiz_id = $1";
+        const resultInsert = `INSERT INTO result(quiz_id, title, description, position)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id;`;
+        const resultDelete = "DELETE FROM result WHERE id = $1 AND quiz_id = $2;";
 
-        // for (const r of results) {
-        //     const resDesc = r.description ?? null;
-        //     if (existingResult.includes(r)) {
-        //         await pool.query(resultUpdate, [
-        //             r.title,
-        //             resDesc,
-        //             r.position
-        // r.id,
-        //             id
-        //         ]);
-        //     }
-        //     else {
-        //         await pool.query(resultInsert, [
-        //             r.title,
-        //             resDesc,
-        //         ]);
-        //     }
-        // }
+        const resultResult = await pool.query(resultQuery, [id]);
+        const existingResult = resultResult.rows.map(item => item.id);
+        const resultIds = results
+        .filter(element => element !== undefined)
+        .map(item => item.id);
 
-        // // TODO: validate unique position for:
-        // // question, option, result
-        // const questionUpdate = `UPDATE question
-        // SET content = $1,
-        // position = $2,
-        // weight = $3,
-        // single_choice = $4
-        // WHERE id = $5
-        // AND quiz_id = $6;`;
+        for (const r in existingResult) {
+            if (!resultIds.includes(r)) {
+                await pool.query(resultDelete, [
+                    r,
+                    id
+                ]);
+            }
+        }
 
-        // const optionUpdate = `UPDATE option
-        // SET content = $1,
-        // position = $2
-        // WHERE id = $3
-        // AND question_id = $4`;
+        
+        let resultIdArray = [];
+        for (const r of results) {
+            const resDesc = r.description ?? null;
+            let newResultId;
+            if (r.id !== undefined) {
+                await pool.query(resultUpdate, [
+                    r.title,
+                    resDesc,
+                    r.position,
+                    r.id,
+                    id
+                ]);
+            }
+            else {
+                newResultId = await pool.query(resultInsert, [
+                    id,
+                    r.title,
+                    resDesc,
+                    r.position
+                ]);
+            }
+            
+            const resultId = newResultId ?? r.id
+            resultIdArray.splice(r.position, 0, resultId);
+        }
+        
 
-        // for (const q of questions) {
-        //     await pool.query(questionUpdate, [
-        //         q.content,
-        //         q.position,
-        //         q.weight,
-        //         q.single_choice,
-        //         q.id,
-        //         id
-        //     ]);
+        // TODO: validate unique position for:
+        // question, option, result
+        const questionUpdate = `UPDATE question
+        SET content = $1,
+        position = $2,
+        weight = $3,
+        single_choice = $4
+        WHERE id = $5
+        AND quiz_id = $6;`;
+        const questionInsert = `INSERT INTO question(quiz_id, content, position, weight, single_choice)
+        VALUES ($1, $2, $3, $4, $5);`;
+        const questionQuery = "SELECT id FROM question WHERE quiz_id = $1;";
+        const questionDelete = "DELETE FROM question WHERE id = $1 AND quiz_id = $2;";
 
-        for (const o of q.options) {
-            // await pool.query(optionUpdate, [
-            //     o.content,
-            //     o.position,
-            //     o.id,
-            //     q.position
-            // ]);
+        const optionUpdate = `UPDATE option
+        SET content = $1,
+        position = $2
+        WHERE id = $3
+        AND question_id = $4;`;
+        const optionInsert = `INSERT INTO option(question_id, content, position)
+        VALUES ($1, $2, $3);`;
+        const optionQuery = "SELECT id FROM option WHERE question_id = $1;";
+        const optionDelete = "DELETE FROM option WHERE id = $1 AND question_id = $2;";
 
-            const optionResultInsert = `INSERT INTO option_result(option_id, result_id)
+        const resQuiz = await pool.query(questionQuery, [id]);
+        const existingQuestions = resQuiz.rows.map(item => item.id);
+        const questionIds = questions
+        .filter(element => element !== undefined)
+        .map(item => item.id);
+
+        for (const q in existingQuestions) {
+            if (!questionIds.includes(q)) {
+                await pool.query(questionDelete, [
+                    q,
+                    id
+                ]);
+            }
+        }
+
+        for (const q of questions) {
+            
+            if (q.quiz_id !== id) {
+                continue;
+            }
+            
+            if (r.id !== undefined) {
+                await pool.query(questionUpdate, [
+                    q.content,
+                    q.position,
+                    q.weight,
+                    q.single_choice
+                ]);
+            }
+            else {
+                await pool.query(questionInsert, [
+                    id,
+                    q.content,
+                    q.position,
+                    q.weight,
+                    q.single_choice
+                ]);
+            }
+
+            const resOptions = await pool.query(optionQuery, [q.id]);
+            const existingOptions = resOptions.rows.map(item => item.id);
+            const optionIds = q.options.rows.map(item => item.id);
+            
+            for (const o in existingOptions) {
+                if (!optionIds.includes(o)) {
+                    await pool.query(optionDelete, [
+                        o.id,
+                        q.id
+                    ]);
+                }
+            }
+            
+            for (const o of q.options) {
+                
+                if (o.id !== undefined) {
+                    await pool.query(optionUpdate, [
+                        o.content,
+                        o.position,
+                        o.id,
+                        q.position
+                    ]);
+                }
+                else {
+                    await pool.query(optionInsert, [
+                        q.position,
+                        o.content,
+                        o.position
+                    ]);
+                }
+
+                const optionResultInsert = `INSERT INTO option_result(option_id, result_id)
                 VALUES ($1, $2);`;
-            const optionResultDelete = `DELETE FROM option_result
+                const optionResultDelete = `DELETE FROM option_result
                 WHERE option_id = $1
                 AND result_id = $2;`;
 
-            const optionResultQuery = "SELECT result_id FROM option_result WHERE option_id = $1";
-            const optionResultResult = await pool.query(optionResultQuery, [o.id]);
-            const existingResults = optionResultResult.rows.map(item => item.result_id);
+                const optionResultQuery = "SELECT result_id FROM option_result WHERE option_id = $1";
+                const optionResultResult = await pool.query(optionResultQuery, [o.id]);
+                //result_ids
+                const existingResults = optionResultResult.rows.map(item => item.result_id);
+                //resultIdArray has ids, index is position
+                // TODO: fix for loops below?
+                // are they working?
+                
+                const resultIds = o.result_ids.rows;
 
-            for (const r of o.result_ids) {
-                if (!existingResults.includes(r)) {
-                    await pool.query(optionResultInsert, [o.id, r]);
+                for (const r of existingResults) {
+                    if (!resultIds.includes(r)) {
+                        await pool.query(optionResultDelete, [o.id, r]);
+                    }
                 }
-            }
+                
+                for (const r of resultIds) {
+                    if (!existingResults.includes(r)) {
+                        await pool.query(optionResultInsert, [o.id, r]);
+                    }
+                }
 
-            for (const r of existingResults) {
-                if (!o.result_ids.includes(r)) {
-                    await pool.query(optionResultDelete, [o.id, r]);
-                }
             }
         }
-        // }
 
         await pool.query('COMMIT');
         return getQuizById(id);
@@ -268,7 +357,6 @@ const updateQuizById = async (id, quiz) => {
         console.error(error.message);
         throw error;
     }
-
 }
 
 const deleteQuizById = async (id) => {
