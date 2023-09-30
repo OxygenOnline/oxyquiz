787c086a7cfda289d8e3b5c140de77001600674a
Farkas Fruzsina Diána <fruzsinadiana@mailbox.unideb.hu>
2023-09-30 11:19:49 +0200

    db setup fixed

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,7 @@
 const { Pool } = require('pg');
+const dbconfig = require('../config/config');
 
-const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'oxyquiz'
-});
+const pool = new Pool(dbconfig);
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
