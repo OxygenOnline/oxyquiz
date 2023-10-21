5da7a0c9ab749d26eb6eefaf612d36358683bdf5
Farkas Fruzsina Diána <fruzsinadiana@mailbox.unideb.hu>
2023-10-21 09:19:48 +0200

    fixed seeders

--- server/database/seeders/20231014101734-test_users.js
+++ server/database/seeders/20231014101734-test_users.js
@@ -8,28 +8,28 @@ module.exports = {
       username: "OxygenOnline",
       password: "SafePassword",
       email: "oxygenonline@gmail.com",
-      joining_date: Sequelize.fn("NOW")
+      joiningDate: Sequelize.fn("NOW")
     },
     {
       id: 1002,
       username: "habuma",
       password: "Password",
       email: "habuma@gmail.com",
-      joining_date: Sequelize.fn("NOW")
+      joiningDate: Sequelize.fn("NOW")
     },
     {
       id: 1003,
       username: "new_user123",
       password: "dontremember",
       email: "anewemail@gmail.com",
-      joining_date: Sequelize.fn("NOW")
+      joiningDate: Sequelize.fn("NOW")
     },
     {
       id: 1004,
       username: "blobblob001",
       password: "myPasswordIsSafer123",
       email: "blobblob001@gmail.com",
-      joining_date: Sequelize.fn("NOW")
+      joiningDate: Sequelize.fn("NOW")
     }
     ], {});
   },
--- server/database/seeders/20231014101749-test_quizzes.js
+++ server/database/seeders/20231014101749-test_quizzes.js
@@ -6,68 +6,68 @@ module.exports = {
     await queryInterface.bulkInsert('quizzes', [{
       id: 1001,
       title: "Are you a cat or a dog?",
-      category_id: 6,
-      creator_id: 1001,
-      created_at: Sequelize.fn("NOW"),
-      updated_at: Sequelize.fn("NOW")
+      categoryId: 6,
+      creatorId: 1001,
+      createdAt: Sequelize.fn("NOW"),
+      updatedAt: Sequelize.fn("NOW")
     },
     {
       id: 1002,
       title: "Which Harry Potter house suits you?",
-      category_id: 3,
-      creator_id: 1003,
-      created_at: Sequelize.fn("NOW"),
-      updated_at: Sequelize.fn("NOW")
+      categoryId: 3,
+      creatorId: 1003,
+      createdAt: Sequelize.fn("NOW"),
+      updatedAt: Sequelize.fn("NOW")
     },
     {
       id: 1003,
       title: "Would you survive a zombie apocalypse?",
-      category_id: 9,
-      creator_id: 1003,
-      created_at: Sequelize.fn("NOW"),
-      updated_at: Sequelize.fn("NOW")
+      categoryId: 9,
+      creatorId: 1003,
+      createdAt: Sequelize.fn("NOW"),
+      updatedAt: Sequelize.fn("NOW")
     }
     ], {});
 
     await queryInterface.bulkInsert('questions', [{
       id: 1001,
-      quiz_id: 1001,
+      quizId: 1001,
       content: "Are you a morning person or a night owl?",
       position: 1
     },
     {
       id: 1002,
-      quiz_id: 1001,
+      quizId: 1001,
       content: "Favorite color?",
       position: 2
     },
     {
       id: 1003,
-      quiz_id: 1001,
+      quizId: 1001,
       content: "Favorite subject?",
       position: 0
     },
     {
       id: 1004,
-      quiz_id: 1002,
+      quizId: 1002,
       content: "Do you prefer the seaside, forest or the mountains?",
       position: 1
     },
     {
       id: 1005,
-      quiz_id: 1002,
+      quizId: 1002,
       content: "Favorite pet?",
       position: 0
     },
     {
       id: 1006,
-      quiz_id: 1003,
+      quizId: 1003,
       content: "Were you good at PE?",
       position: 0
     },
     {
       id: 1007,
-      quiz_id: 1003,
+      quizId: 1003,
       content: "Do you think you would survive?",
       position: 1
     }
@@ -75,49 +75,49 @@ module.exports = {
 
     await queryInterface.bulkInsert('results', [{
       id: 1001,
-      quiz_id: 1001,
+      quizId: 1001,
       title: "Cat",
       position: 0
     },
     {
       id: 1002,
-      quiz_id: 1001,
+      quizId: 1001,
       title: "Dog",
       position: 1
     },
     {
       id: 1003,
-      quiz_id: 1002,
+      quizId: 1002,
       title: "Ravenclaw",
       position: 0
     },
     {
       id: 1004,
-      quiz_id: 1002,
+      quizId: 1002,
       title: "Hufflepuff",
       position: 1
     },
     {
       id: 1005,
-      quiz_id: 1002,
+      quizId: 1002,
       title: "Gryffindor",
       position: 2
     },
     {
       id: 1006,
-      quiz_id: 1002,
+      quizId: 1002,
       title: "Slytherin",
       position: 3
     },
     {
       id: 1007,
-      quiz_id: 1003,
+      quizId: 1003,
       title: "Yes, you would survive",
       position: 0
     },
     {
       id: 1008,
-      quiz_id: 1003,
+      quizId: 1003,
       title: "No, you would die",
       position: 1
     }
@@ -125,207 +125,207 @@ module.exports = {
 
     await queryInterface.bulkInsert('options', [{
       id: 1001,
-      question_id: 1001,
+      questionId: 1001,
       content: "Morning person",
       position: 0
     },
     {
       id: 1002,
-      question_id: 1001,
+      questionId: 1001,
       content: "Night owl",
       position: 1
     },
     {
       id: 1003,
-      question_id: 1002,
+      questionId: 1002,
       content: "Red",
       position: 1
     },
     {
       id: 1004,
-      question_id: 1002,
+      questionId: 1002,
       content: "Blue",
       position: 2
     },
     {
       id: 1005,
-      question_id: 1002,
+      questionId: 1002,
       content: "Yellow",
       position: 0
     },
     {
       id: 1006,
-      question_id: 1003,
+      questionId: 1003,
       content: "Math",
       position: 1
     },
     {
       id: 1007,
-      question_id: 1003,
+      questionId: 1003,
       content: "Literature",
       position: 2
     },
     {
       id: 1008,
-      question_id: 1003,
+      questionId: 1003,
       content: "P.E.",
       position: 3
     },
     {
       id: 1009,
-      question_id: 1003,
+      questionId: 1003,
       content: "Arts",
       position: 0
     },
     {
       id: 1010,
-      question_id: 1004,
+      questionId: 1004,
       content: "Seaside",
       position: 1
     },
     {
       id: 1011,
-      question_id: 1004,
+      questionId: 1004,
       content: "Forest",
       position: 2
     },
     {
       id: 1012,
-      question_id: 1004,
+      questionId: 1004,
       content: "Mountain",
       position: 0
     },
     {
       id: 1013,
-      question_id: 1005,
+      questionId: 1005,
       content: "Dog",
       position: 1
     },
     {
       id: 1014,
-      question_id: 1005,
+      questionId: 1005,
       content: "Cat",
       position: 2
     },
     {
       id: 1015,
-      question_id: 1005,
+      questionId: 1005,
       content: "Something exotic",
       position: 0
     },
     {
       id: 1016,
-      question_id: 1006,
+      questionId: 1006,
       content: "Yes",
       position: 1
     },
     {
       id: 1017,
-      question_id: 1006,
+      questionId: 1006,
       content: "No",
       position: 0
     },
     {
       id: 1018,
-      question_id: 1007,
+      questionId: 1007,
       content: "Yes",
       position: 1
     },
     {
       id: 1019,
-      question_id: 1007,
+      questionId: 1007,
       content: "No",
       position: 0
     }
     ], {});
 
     await queryInterface.bulkInsert('option_results', [{
-      option_id: 1001,
-      result_id: 1002
+      optionId: 1001,
+      resultId: 1002
     },
     {
-      option_id: 1002,
-      result_id: 1001
+      optionId: 1002,
+      resultId: 1001
     },
     {
-      option_id: 1003,
-      result_id: 1001
+      optionId: 1003,
+      resultId: 1001
     },
     {
-      option_id: 1003,
-      result_id: 1002
+      optionId: 1003,
+      resultId: 1002
     },
     {
-      option_id: 1004,
-      result_id: 1002
+      optionId: 1004,
+      resultId: 1002
     },
     {
-      option_id: 1005,
-      result_id: 1001
+      optionId: 1005,
+      resultId: 1001
     },
     {
-      option_id: 1006,
-      result_id: 1001
+      optionId: 1006,
+      resultId: 1001
     },
     {
-      option_id: 1007,
-      result_id: 1002
+      optionId: 1007,
+      resultId: 1002
     },
     {
-      option_id: 1008,
-      result_id: 1002
+      optionId: 1008,
+      resultId: 1002
     },
     {
-      option_id: 1009,
-      result_id: 1001
+      optionId: 1009,
+      resultId: 1001
     },
     {
-      option_id: 1010,
-      result_id: 1006
+      optionId: 1010,
+      resultId: 1006
     },
     {
-      option_id: 1011,
-      result_id: 1005
+      optionId: 1011,
+      resultId: 1005
     },
     {
-      option_id: 1011,
-      result_id: 1004
+      optionId: 1011,
+      resultId: 1004
     },
     {
-      option_id: 1012,
-      result_id: 1003
+      optionId: 1012,
+      resultId: 1003
     },
     {
-      option_id: 1013,
-      result_id: 1005
+      optionId: 1013,
+      resultId: 1005
     },
     {
-      option_id: 1013,
-      result_id: 1004
+      optionId: 1013,
+      resultId: 1004
     },
     {
-      option_id: 1014,
-      result_id: 1006
+      optionId: 1014,
+      resultId: 1006
     },
     {
-      option_id: 1015,
-      result_id: 1003
+      optionId: 1015,
+      resultId: 1003
     },
     {
-      option_id: 1016,
-      result_id: 1007
+      optionId: 1016,
+      resultId: 1007
     },
     {
-      option_id: 1017,
-      result_id: 1008
+      optionId: 1017,
+      resultId: 1008
     },
     {
-      option_id: 1018,
-      result_id: 1007
+      optionId: 1018,
+      resultId: 1007
     },
     {
-      option_id: 1019,
-      result_id: 1008
+      optionId: 1019,
+      resultId: 1008
     }
 
     ], {});
