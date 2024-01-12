9269e7416fe7c3619c23bb593f4ed0aff9e1db77
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-12 10:07:52 +0100

    whole card is link to quiz

--- client/app/components/QuizCard.js
+++ client/app/components/QuizCard.js
@@ -6,19 +6,17 @@ const QuizCard = (props) => {
     const { quizData } = props;
 
     return (
-        <div className='container h-fit flex-1 text-center p-6'>
-            <div className='underlined pb-4'>
-                <Link href={`/quizzes/${quizData.id}`}>
+        <Link href={`/quizzes/${quizData.id}`}>
+            <div className='container h-fit flex-1 text-center p-6'>
+                <div className='underlined pb-4'>
                     <h1 className=' text-2xl font-semibold '>{quizData.title}</h1>
-                </Link>
+                </div>
+                <p className='accent-colored lowercase'>{quizData.category.name}</p>
+                {quizData.description && (<h2 className='px-12 pt-8'>{quizData.description}</h2>)}
+                <p className='accent-colored pt-6'>by {quizData.creator.username}</p>
+                <p className='accent-colored' >{quizData.createdAt.split('T')[0]}</p>
             </div>
-            <Link href={`/categories/${quizData.category.pathName}`} className='accent-colored lowercase'>
-                {quizData.category.name}
-            </Link>
-            {quizData.description && (<h2 className='px-12 pt-8'>{quizData.description}</h2>)}
-            <p className='accent-colored pt-6'>by {quizData.creator.username}</p>
-            <p className='accent-colored' >{quizData.createdAt.split('T')[0]}</p>
-        </div>
+        </Link >
     );
 };
 
