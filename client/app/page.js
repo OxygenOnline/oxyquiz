14e188bc6a23f8e8f2a91ff7401d5c2414eab10c
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-18 10:48:39 +0100

    landing page created

--- client/app/page.js
+++ client/app/page.js
@@ -1,6 +1,47 @@
-export default function Home() {
+import { getQuizzes } from './api';
+import QuizGrid from './components/QuizGrid';
+import WelcomeSection from './components/LandingPage/WelcomeSection';
+import RandomSection from './components/LandingPage/RandomSection';
+import CategorySection from './components/LandingPage/CategorySection';
+import RegisterSection from './components/LandingPage/RegisterSection';
+
+
+const fetchQuizzes = async () => {
+  try {
+    const response = await getQuizzes();
+
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+const Home = async () => {
+
+  const quizzes = await fetchQuizzes();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex flex-col items-center justify-between p-8 lg:p-12'>
+
+      <WelcomeSection />
+
+      {quizzes && (
+        <section className='text-center pt-12 md:pt-20 md:pb-24 pb-16'>
+          <h2 className='accent-colored text-4xl font-bold mb-12'>Hottest Selection</h2>
+          <QuizGrid quizzes={quizzes} />
+        </section>
+      )}
+
+      <RandomSection />
+
+      <CategorySection />
+
+      <RegisterSection />
+
     </main>
   )
-}
+};
+
+export default Home;
