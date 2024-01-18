4d5295930f97c22e0c8665f8aa591ecd9ecfdc23
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-18 10:39:16 +0100

    category pages implemented

---
+++ client/app/categories/[category]/page.js
@@ -0,0 +1,39 @@
+import { notFound } from 'next/navigation';
+import { getCategoryQuizzes } from '../../api';
+import QuizBrowser from '../../components/QuizBrowser';
+
+
+const fetchQuizzes = async (categoryName, page) => {
+  
+  try {
+    const response = await getCategoryQuizzes(categoryName, page);
+
+    if (response.status === 404) {
+      return undefined;
+    }
+
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+const CategoryPage = async ({ params, searchParams }) => {
+
+  const page = searchParams['page'] ?? '0';
+  const quizzes = await fetchQuizzes(params.category, page);
+
+  if (!quizzes) {
+    notFound();
+  }
+
+  return (
+    <main className='m-8 justify-center lg:mx-12 lg:my-16'>
+      <QuizBrowser quizzes={quizzes} />
+    </main >
+  );
+};
+
+export default CategoryPage;
