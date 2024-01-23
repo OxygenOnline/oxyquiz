a171e03744b7dc30f2fc9b12d17a1243b4890ce8
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-23 19:55:02 +0100

    padding adjusted for description

--- client/app/components/QuizCard.js
+++ client/app/components/QuizCard.js
@@ -15,7 +15,7 @@ const QuizCard = (props) => {
                     <h1 className='text-2xl font-semibold'>{quizData.title}</h1>
                 </div>
                 <p className='accent-colored lowercase'>{quizData.category.name}</p>
-                {quizData.description && (<h2 className='px-12 pt-8'>{quizData.description}</h2>)}
+                {quizData.description && (<h2 className='px-2 pt-8'>{quizData.description}</h2>)}
                 <p className='accent-colored pt-6'>by {quizData.creator.username}</p>
                 <p className='accent-colored'>{quizData.createdAt.split('T')[0]}</p>
             </div>
