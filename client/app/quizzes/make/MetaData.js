ea795a52c71470e1b3a067a84a583b253673dbc3
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-23 20:03:46 +0100

    added section for updated quizzes

--- client/app/quizzes/make/MetaData.js
+++ client/app/quizzes/make/MetaData.js
@@ -29,54 +29,63 @@ const MetaData = () => {
     };
 
     return (
-        <div className='container p-3 mb-6'>
+        <>
             {quiz.id && (
-                <div className='underlined'>
-                    <Link
-                        href={`/quizzes/${quiz.id}`}
-                        className='text-xl font-bold accent-colored'
-                    >
-                        View The Published Quiz
-                    </Link>
+                <div className='container text-center p-3 mb-6'>
+                    <div className='underlined'>
+                        <Link
+                            href={`/quizzes/${quiz.id}`}
+                            className='text-xl font-bold accent-colored'
+                        >
+                            View The Published Quiz
+                        </Link>
+                    </div>
+                    <p className='text-lg mt-6'>Id: [{quiz.id}]</p>
+                    <p className='text-lg'>Created: {quiz.createdAt.split('T')[0]}</p>
+                    {quiz.createdAt !== quiz.updatedAt && (
+                        <p className='text-lg'>Updated: {quiz.updatedAt.split('T')[0]}</p>
+                    )}
                 </div>
             )}
-            < div className='flex flex-col mt-6'>
-                <label className='text-xl font-bold'>Quiz Title</label>
-                <input
-                    className='p-1'
-                    type="text"
-                    value={quiz.title}
-                    onChange={handleTitleChange}
-                    required
-                />
+            <div className='container p-3 mb-6'>
+                <div className='flex flex-col'>
+                    <label className='text-xl font-bold'>Quiz Title</label>
+                    <input
+                        className='p-1'
+                        type="text"
+                        value={quiz.title}
+                        onChange={handleTitleChange}
+                        required
+                    />
+                </div>
+                <div className='flex flex-col mt-6'>
+                    <label className='text-xl font-bold'>Description</label>
+                    <textarea
+                        className='p-1'
+                        value={quiz.description}
+                        onChange={handleDescriptionChange}
+                    />
+                </div>
+                <div className='flex flex-col mt-6'>
+                    <label className='text-xl font-bold'>Category</label>
+                    <select
+                        className='py-1'
+                        value={quiz.categoryId}
+                        onChange={handleCategoryChange}
+                        required>
+                        <option value=''></option>
+                        {categories.map((category) => (
+                            <option
+                                key={category.id}
+                                value={category.id}
+                            >
+                                {category.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div className='flex flex-col mt-6'>
-                <label className='text-xl font-bold'>Description</label>
-                <textarea
-                    className='p-1'
-                    value={quiz.description}
-                    onChange={handleDescriptionChange}
-                />
-            </div>
-            <div className='flex flex-col mt-6'>
-                <label className='text-xl font-bold'>Category</label>
-                <select
-                    className='py-1'
-                    value={quiz.categoryId}
-                    onChange={handleCategoryChange}
-                    required>
-                    <option value=''></option>
-                    {categories.map((category) => (
-                        <option
-                            key={category.id}
-                            value={category.id}
-                        >
-                            {category.name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-        </div>
+        </>
     );
 };
 
