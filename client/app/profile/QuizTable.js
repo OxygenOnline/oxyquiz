b04182c9a8b141af7c9c21e801d781313706a312
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-23 19:44:15 +0100

    fixed pagination navigator

--- client/app/profile/QuizTable.js
+++ client/app/profile/QuizTable.js
@@ -22,7 +22,7 @@ const QuizTable = ({ quizzes }) => {
 
     return (
         <div className='container flex flex-col min-h-40 py-6 items-center justify-center mb-12'>
-            {!quizzes || quizzes.length === 0 ? (
+            {!quizzes || quizzes.length === 0 && page <= 0 ? (
                 <p className='text-center text-xl font-semibold'>No quizzes available</p>
             ) : (
                 <>
