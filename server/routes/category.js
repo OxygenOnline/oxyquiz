0e47ab59583c8ff2addd23fef6f36f0020b480be
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-01-28 10:23:34 +0100

    logging added to category endpoint

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/category');
+const logger = require('../utils/logger');
+
+
+router.use((req, res, next) => {
+    logger.http(`[${req.method}] ${req.originalUrl}`);
+    next();
+});
 
 router.route('/')
     .get(controller.getAllCategories);
