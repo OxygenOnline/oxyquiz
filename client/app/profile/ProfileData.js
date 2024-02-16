ad6502ae9e406ad118bfe17c1e7df12fd2dce7dd
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2024-02-16 21:37:05 +0100

    profile card adjusted for smaller screens

--- client/app/profile/ProfileData.js
+++ client/app/profile/ProfileData.js
@@ -1,11 +1,23 @@
 const ProfileData = ({ profile }) => {
 
     return (
-        <div className='container h-fit p-6 text-lg'>
-            <h2>username: {profile?.username}</h2>
-            <p>id: {profile?.id}</p>
-            <p>email: {profile?.email}</p>
-            <p>joining date: {profile?.joiningDate?.split('T')[0]}</p>
+        <div className='container h-fit p-6 text-lg flex flex-col'>
+            <div className='flex flex-col md:flex-row mb-2'>
+                <p className='md:mr-2'>Username:</p>
+                <p className='break-words'>{profile?.username}</p>
+            </div>
+            <div className='flex flex-col md:flex-row mb-2'>
+                <p className='md:mr-2'>ID:</p>
+                <p>{profile?.id}</p>
+            </div>
+            <div className='flex flex-col md:flex-row mb-2'>
+                <p className='md:mr-2'>Email:</p>
+                <p className='break-words'>{profile?.email}</p>
+            </div>
+            <div className='flex flex-col md:flex-row'>
+                <p className='md:mr-2'>Joining Date:</p>
+                <p>{profile?.joiningDate?.split('T')[0]}</p>
+            </div>
         </div>
     );
 };
